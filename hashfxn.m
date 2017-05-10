978aed46e4ac9641399ee70efea5f91134b42eca
xxKanade <xxkristinap@gmail.com>
2017-05-10 02:15:25 -0400

    hash and search function
    Added hash function and updated search function

---
+++ hashfxn.m
@@ -0,0 +1,7 @@
+% hashfxn() calculates a location by taking in three parameters, the peak
+% frequency, DT (IDK WHAT THIS IS), and SIZE(IDK WHAT THIS IS PLS DEFINE).
+function hashme = hashfxn(pk, dt, size)
+    % define this thing below
+    hash = mod(round(), size)+1;
+
+end
--- search.m
+++ search.m
@@ -1,8 +1,18 @@
+% match() has input arguments sound clip z and sampling frequency Fs.
+% With these parameters match() attempts to find a matching song within
+% songDatabase
+
 function match = search(z, Fs)
+    % global variables
+    global songDatabase
+    global ttlSongs
+
     [s,w,t] = spectrogram(z);
     Y = fft(z);                      % fft with N = 5000 samples
 
-
+    matches = cell(ttlSongs, 1)
+    for k = 1:size(key, 1)
+        hash = hashme
 
 
 
